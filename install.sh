c50f4400b45a91368168d4fe49f820948caca883
Konstantin Dietrich <konst.dietrich@gmail.com>
2025-08-21 12:58:38 +0200

    Got rid of pflacco integration

--- install.sh
+++
@@ -1,8 +0,0 @@
-python -m pip uninstall pflacco --yes
-python -m pip install -r requirements.txt
-git clone https://github.com/Reiyan/pflacco.git
-cp -rf MANIFEST.in pflacco/
-cp -rf setup.py pflacco/
-cp -r deep_ela pflacco/pflacco
-cd pflacco
-python -m pip install .
