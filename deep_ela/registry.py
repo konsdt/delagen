45d1a01789f5f0c9691eb5623b16e80ce326f231
Konstantin Dietrich <konst.dietrich@gmail.com>
2025-08-21 13:00:08 +0200

    make deep_ela weights downloadable so we dont need to distribute them in the binary

---
+++ deep_ela/registry.py
@@ -0,0 +1,20 @@
+# Map a friendly model name -> public URL of the checkpoint
+MODELS = {
+    "large-25d-v1": {
+        "ckpt_url": "https://github.com/konsdt/deep_ela/releases/download/testing/epoch.249-step.65750_backbone-large-25d-v1.ckpt",
+        "hparams_url": "https://github.com/konsdt/deep_ela/releases/download/testing/hparams-large-25d-v1.yaml"
+    },
+    "large-50d-v1": {
+        "ckpt_url": "https://github.com/konsdt/deep_ela/releases/download/testing/epoch.249-step.78250_backbone-large-50d-v1.ckpt",
+        "hparams_url": "https://github.com/konsdt/deep_ela/releases/download/testing/hparams-large-50d-v1.yaml"
+    },
+   "medium-25d-v1": {
+        "ckpt_url": "https://github.com/konsdt/deep_ela/releases/download/testing/epoch.249-step.65750_backbone-medium-25d-v1.ckpt",
+        "hparams_url": "https://github.com/konsdt/deep_ela/releases/download/testing/hparams-medium-25d-v1.yaml"
+    },
+    "medium-50d-v1": {
+        "ckpt_url": "https://github.com/konsdt/deep_ela/releases/download/testing/epoch.249-step.65750_backbone-large-25d-v1.ckpt",
+        "hparams_url": "https://github.com/konsdt/deep_ela/releases/download/testing/hparams-large-25d-v1.yaml"
+    },
+}
+
