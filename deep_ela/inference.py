85f57abf7c87e1a5e83c00700e9c05329d899a1f
Konstantin Dietrich <konst.dietrich@gmail.com>
2025-08-23 14:23:52 +0200

    restructured load_model to work with DeepELA wrapper

--- deep_ela/inference.py
+++ deep_ela/inference.py
@@ -7,47 +7,54 @@ import os,  time
 
 from .encoders import EncoderBackbone
 
-class DeepELA(EncoderBackbone):
-    def __init__(self, name, path_dir=None, path_ckpt=None, path_cnfg=None, device='cpu'):
-        self.name = name
-        assert path_dir is not None or (path_ckpt is not None and path_cnfg is not None), \
-            'Either path_dir or path_ckpt and path_config must be provided!'
-        ## Identify checkpoint and config paths
-        if path_dir is not None:
-            try:
-                path_ckpt = glob(os.path.join(path_dir, '*_backbone.ckpt'))[0]
-                path_cnfg = glob(os.path.join(path_dir, 'hparams.yaml'))[0]
-            except:
-                raise Exception(f'Failed to load model at location {path_dir}!')
-        ## Load config and create model
-        with open(path_cnfg) as f:
-            config = yaml.safe_load(f.read())
-        super().__init__(**config)
-        ## Load parameters
-        import torch
-        self.load_state_dict(torch.load(path_ckpt, weights_only=True)) 
-        ## Set device and eval
-        self.to(device).eval()
-        
-    def __call__(self, X, y, include_costs=False, repetitions=10):
-        start = time.time() # Measure runtime
-        features = super().predict(coordinates=X, fvalues=y, repetitions=repetitions, return_embeddings=False)
-        features = {f'{self.name}.X{i}': f for i,f in enumerate(features)}
-        if include_costs:
-            features[f'{self.name}.costs_runtime'] = time.time() - start
-        return features
-
+# create .cache/torch/checkpoints to store downloaded model weights
 def _cache_dir():
+    # lazy loading
+    # FIXME: when does it really make sense?
     from torch.hub import get_dir
     from pathlib import Path
     d = Path(get_dir()) / "checkpoints"
     d.mkdir(parents=True, exist_ok=True)
     return d
 
-def _ensure_hparams(dst, url: str):
+def _ensure_file(dst, url: str):
     if not dst.exists():
         from torch.hub import download_url_to_file
         download_url_to_file(url, str(dst), progress=True)
+
+class DeepELA(EncoderBackbone):
+    def __init__(self, name, path_ckpt=None, path_cnfg=None, device='cpu'):
+        self.name = name
+        assert (path_ckpt is not None and path_cnfg is not None), \
+            'path_ckpt and path_config must be provided!'
+        ## Identify checkpoint and config paths
+        
+        ## Load config and create model
+        # if hparams are not cached yet download them from url
+        
+        import yaml  # lazy import here
+        with open(path_cnfg) as f:
+            hparams = yaml.safe_load(f.read())
+
+        super().__init__(**hparams)
+        ## Load parameters
+
+
+        import torch
+        self.load_state_dict(torch.load(path_ckpt, weights_only=True)) 
+        ## Set device and eval
+        self.to(device).eval()
+    # FIXME: What about the repetitions parameter? We always predict 10 times?
+    def __call__(self, X, y, include_costs=False, repetitions=10):
+        if include_costs:
+            start = time.time() # Measure runtime
+        features = super().predict(coordinates=X, fvalues=y, repetitions=repetitions, return_embeddings=False)
+        features = {f'{self.name}.X{i}': f for i,f in enumerate(features)}
+        if include_costs:
+            features[f'{self.name}.costs_runtime'] = time.time() - start
+        return features
+
+
 
 def load_deepela(name: str = "medium-50d-v1", device: str = "cpu", strict: bool = True):
     if name not in MODELS:
@@ -58,27 +65,14 @@ def load_deepela(name: str = "medium-50d-v1", device: str = "cpu", strict: bool 
 
     # hparams
     hparams_path = cache / f"{name}-hparams.yaml"
-    _ensure_hparams(hparams_path, urls["hparams_url"])
-    import yaml  # lazy import here
-    with open(hparams_path) as f:
-        hparams = yaml.safe_load(f.read())
-
+    _ensure_file(hparams_path, urls["hparams_url"])
+        
     # ckpt
-    from torch.hub import load_state_dict_from_url  # lazy
-    state = load_state_dict_from_url(
-        urls["ckpt_url"],
-        model_dir=str(cache),
-        progress=True,
-        map_location=device,
-    )
+    ckpt_path = cache / f"{name}-weights.ckpt"
+    _ensure_file(ckpt_path, urls["ckpt_url"])
+    
 
-    # 3) build model + load weights
-    from .inference import DeepELA  # import the heavy module only now
     import torch  # lazy
-    model = DeepELA(**hparams)  # or your constructor
-    if isinstance(state, dict) and "state_dict" in state:
-        model.load_state_dict(state["state_dict"], strict=strict)
-    else:
-        model.load_state_dict(state, strict=strict)
-    model.to(device).eval()
+    model = DeepELA(name=name, path_cnfg=hparams_path, path_ckpt=ckpt_path)  # or your constructor
+    
     return model
