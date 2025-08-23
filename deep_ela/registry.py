ddb358b4523bebcae86ddc261ce6875b4ceb41e0
Konstantin Dietrich <konst.dietrich@gmail.com>
2025-08-23 14:24:24 +0200

    corrected registry for medium 50d model

--- deep_ela/registry.py
+++ deep_ela/registry.py
@@ -13,8 +13,8 @@ MODELS = {
         "hparams_url": "https://github.com/konsdt/deep_ela/releases/download/testing/hparams-medium-25d-v1.yaml"
     },
     "medium-50d-v1": {
-        "ckpt_url": "https://github.com/konsdt/deep_ela/releases/download/testing/epoch.249-step.65750_backbone-large-25d-v1.ckpt",
-        "hparams_url": "https://github.com/konsdt/deep_ela/releases/download/testing/hparams-large-25d-v1.yaml"
+        "ckpt_url": "https://github.com/konsdt/deep_ela/releases/download/testing/epoch.249-step.65750_backbone-medium-50d-v1.ckpt",
+        "hparams_url": "https://github.com/konsdt/deep_ela/releases/download/testing/hparams-medium-50d-v1.yaml"
     },
 }
 
